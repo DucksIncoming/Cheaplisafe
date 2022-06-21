40474495e407808d93626ddcecce912e7bd8eb8f
DucksIncoming <ducksincoming@gmail.com>
2022-06-21 13:54:35 -0400

    EntrySensor Setup

---
+++ Components/EntrySensor.py
@@ -0,0 +1,15 @@
+# Used to detect when a door is opened
+# https://simplisafe.com/build-my-system#:~:text=your%20Burglary%20Sensors-,Entry%20Sensor,-show%20more%20info
+
+# Arduino Pin Variables
+PORT = "COM9"
+
+# === Digital ===
+LED = 8
+ULTRASONIC_WRITE = 6
+TRANSMIT = 2
+# === Analog ===
+ULTRASONIC_READ = 2
+
+def EntrySensor():
+    pass
